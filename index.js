8f44f7fd0d2460d18f8496c7e5d83dde854bfa28
madhusudan9332 <madhusudangautam003@gmail.com>
2024-07-03 17:31:54 +0530

    adding middleware

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+function logginReqs(req, res, next) {
+  const startTime = Date.now();
+
+  res.on("finish", () => {
+    const endTime = Date.now();
+    const timeDiff = endTime - startTime;
+
+    console.log(
+      `${new Date(startTime).toLocaleString()} - ${req.method} ${
+        req.url
+      } - ${timeDiff}ms`
+    );
+  });
+
+  next();
+}
+
+app.use(logginReqs);
+
+app.get("/", (req, res) => {
+  res.json({ success: true, results: "Api is working" });
+});
+
+app.listen(3300, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
